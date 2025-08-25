2ae089ace73ed6955717889059d96b85817670b8
HanaFlU <140057156+HanaFlU@users.noreply.github.com>
2025-08-25 14:35:35 +0700

    Update index.js

--- server/app/config/index.js
+++ server/app/config/index.js
@@ -3,7 +3,7 @@ const config = {
         port: process.env.PORT || 3000,
     },
     db: {
-        uri: process.env.MONGODB_URI || "mongodb+srv://tranhantgdd2020:Omcaydoitho2k4@cluster0.cdokk.mongodb.net/quanlymuonsach"
+        uri: process.env.MONGODB_URI
     }
 };
 
